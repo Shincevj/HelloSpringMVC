c69aaca633a3547b92c7cec9fd756f337164dfb1
fxbupt <fxbupt@sina.com>
2017-05-10 11:22:25 +0800

    mybatis-spring

--- src/main/java/org/cims/hellospringmvc/controller/HelloWorldController.java
+++ src/main/java/org/cims/hellospringmvc/controller/HelloWorldController.java
@@ -1,14 +1,25 @@
 package org.cims.hellospringmvc.controller;
 
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.cims.hellospringmvc.services.StudentService;
+import org.cims.hellospringmvc.model.Student;
+
 @Controller
 public class HelloWorldController {
+	private StudentService studentService;
+	
+	public void setStudentService(StudentService studentService){
+		this.studentService = studentService;
+	}
+	
 	@RequestMapping("/hello")
 	public String hello(Model model) {
-		model.addAttribute("greeting", "Hello Spring MVC Ding Ding");
+		List<Student> students = this.studentService.findAllStudents();
+		model.addAttribute("greeting", students);
 		return "helloworld";
 	}
 }
