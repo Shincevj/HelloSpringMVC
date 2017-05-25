30c18e4b91357e348795d2c3676a840c27ff4a9f
fxbupt <fxbupt@sina.com>
2017-05-25 17:58:31 +0800

    remove ModelAttribute

--- src/main/java/org/cims/hellospringmvc/controller/RestfulController.java
+++ src/main/java/org/cims/hellospringmvc/controller/RestfulController.java
@@ -7,7 +7,6 @@ import org.cims.hellospringmvc.services.StudentService;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
