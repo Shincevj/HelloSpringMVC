b019f743a7751ca898692672d87046352483c040
fxbupt <fxbupt@sina.com>
2017-05-25 17:53:58 +0800

    retrieve data from html to mysql

--- src/main/java/org/cims/hellospringmvc/controller/RestfulController.java
+++ src/main/java/org/cims/hellospringmvc/controller/RestfulController.java
@@ -1,11 +1,15 @@
 package org.cims.hellospringmvc.controller;
 
+import java.util.Date;
+
 import org.cims.hellospringmvc.model.Student;
 import org.cims.hellospringmvc.services.StudentService;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class RestfulController {
@@ -16,5 +20,20 @@ public class RestfulController {
   public Student greeting() {
 	Student student = this.studentService.findStudentById(1);
 	return student;
+  }
+  
+  @RequestMapping("/record")
+  public Student recordStudent(@RequestParam("stud_id") Integer stud_id,
+		                       @RequestParam("name") String name,
+		                       @RequestParam("email") String email
+		                       ) {
+	  Student student = new Student();
+	  student.setStudId(stud_id);
+	  student.setName(name);
+	  student.setEmail(email);
+	  student.setDob(new Date());
+	  studentService.insertStudent(student);
+	  Student retrieveStudent = this.studentService.findStudentById(stud_id);
+	  return retrieveStudent;
   }
 }
