2f58b38fc375689e705666442ca3f06941a9862f
fxbupt <fxbupt@sina.com>
2017-06-01 03:01:01 +0800

    remove web.xml

--- src/main/java/org/cims/hellospringmvc/config/ServletConfig.java
+++ src/main/java/org/cims/hellospringmvc/config/ServletConfig.java
@@ -2,13 +2,14 @@ package org.cims.hellospringmvc.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ComponentScan;
-//import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-//import org.springframework.web.servlet.view.InternalResourceViewResolver;
+// import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 import org.thymeleaf.spring4.SpringTemplateEngine;
@@ -18,45 +19,48 @@ import org.thymeleaf.spring4.view.ThymeleafViewResolver;
 @Configuration
 @ComponentScan(basePackages = {"org.cims.hellospringmvc.controller"})
 public class ServletConfig extends WebMvcConfigurerAdapter {
-  
+
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    registry.addResourceHandler("/webjars/**")
+        .addResourceLocations("classpath:/META-INF/resources/webjars/");
   }
-  
+
   @Override
   public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
     configurer.enable();
   }
-  /*
-  @Bean
-  public InternalResourceViewResolver jspViewResolver() {
-    InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-    viewResolver.setPrefix("/WEB-INF/views/");
-    viewResolver.setSuffix(".jsp");
-    return viewResolver;
+
+  @Override
+  public void addViewControllers(ViewControllerRegistry registry) {
+    registry.addViewController("/").setViewName("index");
   }
-  */
+
+  /*
+   * @Bean public InternalResourceViewResolver jspViewResolver() { InternalResourceViewResolver
+   * viewResolver = new InternalResourceViewResolver(); viewResolver.setPrefix("/WEB-INF/views/");
+   * viewResolver.setSuffix(".jsp"); return viewResolver; }
+   */
   @Bean
   public ServletContextTemplateResolver templateResolver() {
-	ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver();
-	templateResolver.setPrefix("/WEB-INF/views/");
-	templateResolver.setSuffix(".html");
-	templateResolver.setTemplateMode("HTML5");
-	return templateResolver;
+    ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver();
+    templateResolver.setPrefix("/WEB-INF/views/");
+    templateResolver.setSuffix(".html");
+    templateResolver.setTemplateMode("HTML5");
+    return templateResolver;
   }
-  
+
   @Bean
   public SpringTemplateEngine templateEngine() {
-	SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-	templateEngine.setTemplateResolver(templateResolver());
-	return templateEngine;
+    SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+    templateEngine.setTemplateResolver(templateResolver());
+    return templateEngine;
   }
-  
+
   @Bean
   public ThymeleafViewResolver viewResolver(SpringTemplateEngine templateEngine) {
-	ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
-	viewResolver.setTemplateEngine(templateEngine);
-	return viewResolver;
+    ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+    viewResolver.setTemplateEngine(templateEngine);
+    return viewResolver;
   }
 }
